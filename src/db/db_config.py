3deebbb192a6a7cb47774d784c449e9c97b3457a
saarsc <scsaar@gmail.com>
2023-08-25 11:18:22 +0300

    Moving DB config to another repo

--- src/db/db_config.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine, MetaData
-from sqlalchemy.orm import sessionmaker, declarative_base
-
-conn = create_engine("sqlite:///songs.db", echo=False)
-Base = declarative_base(conn)
-session = sessionmaker(bind=conn)()
-metadata = MetaData(bind=conn)
-
-metadata.create_all()
